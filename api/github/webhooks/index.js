87136556d285f92418cb300ea732dfe91add31ed
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-10-14 05:29:02 +0000

    Made module exports. Async stuff shouldn't matter

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -4,25 +4,25 @@ const { createNodeMiddleware, createProbot } = require("probot");
 const app = require("../../../app");
 const { createAppAuth } = require("@octokit/auth-app");
 
-(async () => {
-  const { Octokit } = await import("@octokit/rest");
+// (async () => {
+const { Octokit } = import("@octokit/rest");
 
-  const octokit = new Octokit({
-    authStrategy: createAppAuth,
-    auth: {
-      appId: process.env.APP_ID,
-      privateKey: process.env.PRIVATE_KEY, // Ensure newlines are correctly handled
-      clientId: process.env.CLIENT_ID,
-      clientSecret: process.env.CLIENT_SECRET,
-    },
-  });
-
-  const probot = createProbot({
-    id: process.env.APP_ID,
-    secret: process.env.WEBHOOK_SECRET,
+const octokit = new Octokit({
+authStrategy: createAppAuth,
+auth: {
+    appId: process.env.APP_ID,
     privateKey: process.env.PRIVATE_KEY, // Ensure newlines are correctly handled
-    Octokit: octokit,
-  });
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+},
+});
 
-  module.exports = createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks' });
-})();
+const probot = createProbot({
+id: process.env.APP_ID,
+secret: process.env.WEBHOOK_SECRET,
+privateKey: process.env.PRIVATE_KEY, // Ensure newlines are correctly handled
+Octokit: octokit,
+});
+
+module.exports = createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks' });
+// })();
