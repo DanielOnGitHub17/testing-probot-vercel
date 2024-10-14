9a2a5f14584accdf61f80f47c7927e37ea1d0c94
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-10-14 05:34:03 +0000

    Back to this code, I guess :)

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,28 +1,10 @@
-require('dotenv').config();
-
 const { createNodeMiddleware, createProbot } = require("probot");
 const app = require("../../../app");
-const { createAppAuth } = require("@octokit/auth-app");
-
-// (async () => {
-const { Octokit } = import("@octokit/rest");
-
-const octokit = new Octokit({
-authStrategy: createAppAuth,
-auth: {
-    appId: process.env.APP_ID,
-    privateKey: process.env.PRIVATE_KEY, // Ensure newlines are correctly handled
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-},
-});
 
 const probot = createProbot({
-id: process.env.APP_ID,
-secret: process.env.WEBHOOK_SECRET,
-privateKey: process.env.PRIVATE_KEY, // Ensure newlines are correctly handled
-Octokit: octokit,
+  id: process.env.APP_ID,
+  secret: process.env.WEBHOOK_SECRET,
+  privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), // Ensure newlines are correctly handled
 });
 
 module.exports = createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks' });
-// })();
