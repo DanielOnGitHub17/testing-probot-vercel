0b269f845d923a9f0514973d197569f8603f8f32
Daniel Enesi <137162024+DanielOnGitHub17@users.noreply.github.com>
2024-10-14 05:24:16 +0000

    I see... My clientsecret was never set. I only set my webhook secret. I think this is the real issue :)

--- api/github/webhooks/index.js
+++ api/github/webhooks/index.js
@@ -1,18 +1,34 @@
 const { createNodeMiddleware, createProbot } = require("probot");
-
 const app = require("../../../app");
-const { Octokit } = require("@octokit/rest");
 const { createAppAuth } = require("@octokit/auth-app");
 
-const octokit = new Octokit({
-  authStrategy: createAppAuth,
-  auth: {
-    appId: process.env.APP_ID,
-    privateKey: process.env.PRIVATE_KEY,
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.WEBHOOK_SECRET,
-  },
-});
+(async () => {
+  const { Octokit } = await import("@octokit/rest");
 
+  const octokit = new Octokit({
+    authStrategy: createAppAuth,
+    auth: {
+      appId: process.env.APP_ID,
+      privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), // Ensure newlines are correctly handled
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+    },
+  });
 
-module.exports = createNodeMiddleware(app, { octokit, webhooksPath: '/api/github/webhooks' });
+  const probot = createProbot({
+    id: process.env.APP_ID,
+    secret: process.env.WEBHOOK_SECRET,
+    privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), // Ensure newlines are correctly handled
+    Octokit: Octokit.defaults({
+      authStrategy: createAppAuth,
+      auth: {
+        appId: process.env.APP_ID,
+        privateKey: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'),
+        clientId: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+      },
+    }),
+  });
+
+  module.exports = createNodeMiddleware(app, { probot, webhooksPath: '/api/github/webhooks' });
+})();
